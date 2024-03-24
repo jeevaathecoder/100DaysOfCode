15ca2e0f2ed16f3cebe0a51d5cec4fd6f27674e3
jeevaathecoder <jeevaathecoder@gmail.com>
2024-03-24 09:17:53 +0530

    Day 84 Done!

---
+++ src/Day84/Demo.java
@@ -0,0 +1,13 @@
+package Day84;
+
+public class Demo {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        System.out.println("Original Array: ");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+
+    }
+}
