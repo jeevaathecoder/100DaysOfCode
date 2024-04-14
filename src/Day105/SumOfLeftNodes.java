e1df80452e9d76e681afad0a47a5015ea4780a31
jeevaathecoder <jeevaathecoder@gmail.com>
2024-04-14 18:49:44 +0530

    Day 105 Done

---
+++ src/Day105/SumOfLeftNodes.java
@@ -0,0 +1,42 @@
+//package Day105;
+//https://leetcode.com/problems/sum-of-left-leaves/?envType=daily-question&envId=2024-04-14
+//404. Sum of Left Leaves
+//public class SumOfLeftNodes {
+//    /**
+//     * Definition for a binary tree node.
+//     * public class TreeNode {
+//     *     int val;
+//     *     TreeNode left;
+//     *     TreeNode right;
+//     *     TreeNode() {}
+//     *     TreeNode(int val) { this.val = val; }
+//     *     TreeNode(int val, TreeNode left, TreeNode right) {
+//     *         this.val = val;
+//     *         this.left = left;
+//     *         this.right = right;
+//     *     }
+//     * }
+//     */
+//    class Solution {
+//        public int sumOfLeftLeaves(TreeNode root) {
+//            if(root ==null){
+//                return 0;
+//            }
+//            int sum=0;
+//            if(root.left!=null){
+//                if(root.left.left==null && root.left.right==null){
+//                    sum+=root.left.val;
+//
+//                }else{
+//
+//                    sum+=sumOfLeftLeaves(root.left);
+//                }
+//            }
+//
+//            sum+=sumOfLeftLeaves(root.right);
+//            return sum;
+//
+//        }
+//
+//    }
+//}
