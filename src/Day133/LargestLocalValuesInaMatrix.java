7a755ea1f2306ec38898aa229500f122a9ab19cd
jeeva <jeevaathecoder@gmail.com>
2024-05-12 21:00:47 +0530

    Day 133 Done!

---
+++ src/Day133/LargestLocalValuesInaMatrix.java
@@ -0,0 +1,28 @@
+package Day133;
+
+//2373. Largest Local Values in a Matrix
+//https://leetcode.com/problems/largest-local-values-in-a-matrix/
+public class LargestLocalValuesInaMatrix {
+    class Solution {
+        public int[][] largestLocal(int[][] grid) {
+            int n = grid.length;
+            int[][] res = new int[n - 2][n - 2];
+
+            for(int i = 1; i < n - 1; ++i) {
+                for(int j = 1; j < n - 1; ++j) {
+                    int temp = 0;
+
+                    for(int k = i - 1; k <= i + 1; ++k) {
+                        for(int l = j - 1; l <= j + 1; ++l) {
+                            temp = Math.max(temp, grid[k][l]);
+                        }
+                    }
+
+                    res[i - 1][j - 1] = temp;
+                }
+            }
+
+            return res;
+        }
+    }
+}
