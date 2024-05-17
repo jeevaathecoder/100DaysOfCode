1fc73be2c6b1df24ff4eb81fb5d8cf4f94a1cc94
jeeva <jeevaathecoder@gmail.com>
2024-05-17 19:45:29 +0530

    Day 138 Done!

---
+++ src/Day138/DeleteLeavesWithaGivenValue.java
@@ -0,0 +1,21 @@
+//package Day138;
+////1325. Delete Leaves With a Given Value
+////https://leetcode.com/problems/delete-leaves-with-a-given-value/
+//
+
+//
+//public class DeleteLeavesWithaGivenValue {
+//    class Solution {
+//        public TreeNode removeLeafNodes(TreeNode root, int target) {
+//            if(root != null) {
+//                root.left = removeLeafNodes(root.left, target);
+//                root.right = removeLeafNodes(root.right, target);
+//                if(root.left == null && root.right == null && root.val == target){
+//                    return null;
+//                }
+//                return root;
+//            }
+//            return null;
+//        }
+//    }
+//}
