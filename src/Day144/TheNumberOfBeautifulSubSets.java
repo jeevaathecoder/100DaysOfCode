a15463b511103f1f3c371722f2e1567b5f151fb3
jeeva <jeevaathecoder@gmail.com>
2024-05-23 17:18:16 +0530

    Day 144 Done!

---
+++ src/Day144/TheNumberOfBeautifulSubSets.java
@@ -0,0 +1,29 @@
+//package Day144;
+//https://leetcode.com/problems/the-number-of-beautiful-subsets/
+//public class TheNumberOfBeautifulSubSets {
+//    class Solution {
+//        public int dfs(int[] nums, int idx, int k, HashMap<Integer, Integer> mp)    {
+//            if (idx == nums.length) return 1;
+//
+//            int taken = 0;
+//            if (!mp.containsKey(nums[idx] - k) && !mp.containsKey(nums[idx] + k)) {
+//                mp.put(nums[idx], mp.getOrDefault(nums[idx], 0) + 1);
+//                taken = dfs(nums, idx + 1, k, mp);
+//                mp.put(nums[idx], mp.get(nums[idx]) - 1);
+//                if (mp.get(nums[idx]) == 0) {
+//                    mp.remove(nums[idx]);
+//                }
+//            }
+//
+//            int notTaken = dfs(nums, idx + 1, k, mp);
+//
+//            return taken + notTaken;
+//        }
+//
+//        public int beautifulSubsets(int[] nums, int k) {
+//            HashMap<Integer, Integer> mp = new HashMap<>();
+//            int ans = dfs(nums, 0, k, mp);
+//            return ans - 1;
+//        }
+//    }
+//}
