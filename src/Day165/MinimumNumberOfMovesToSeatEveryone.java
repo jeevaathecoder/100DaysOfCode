9ff50fc67f99ffc5bf80e19cdb87325f1e6e0e15
jeeva <jeevaathecoder@gmail.com>
2024-06-13 19:42:36 +0530

    Day 165 Done!

---
+++ src/Day165/MinimumNumberOfMovesToSeatEveryone.java
@@ -0,0 +1,18 @@
+package Day165;
+
+import java.util.Arrays;
+
+public class MinimumNumberOfMovesToSeatEveryone {
+    class Solution {
+        public int minMovesToSeat(int[] seats, int[] students) {
+            Arrays.sort(seats);
+            Arrays.sort(students);
+            int n=seats.length;
+            int sum=0;
+            for(int i=0;i<n;i++){
+                sum += Math.abs(seats[i]-students[i]);
+            }
+            return sum;
+        }
+    }
+}
