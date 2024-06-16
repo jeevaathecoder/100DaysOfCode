985dd6ca5d63978a9f268cd61257adc4115b202f
jeeva <jeevaathecoder@gmail.com>
2024-06-16 17:43:58 +0530

    Day 168 Done!

---
+++ src/Day168/PatchingArray.java
@@ -0,0 +1,22 @@
+package Day168;
+
+public class PatchingArray {
+    class Solution {
+        public int minPatches(int[] nums, int n) {
+            int count = 0;
+            long missing = 1;
+            int i = 0;
+
+            while (missing <= n) {
+                if (i < nums.length && nums[i] <= missing) {
+                    missing += nums[i++];
+                } else {
+                    missing += missing;
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
